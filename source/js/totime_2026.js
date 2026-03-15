7b7bb14383e5e0d7c76845e07da787274da0c896
duoduo13123678 <hellomc2333@163.com>
2026-03-15 08:25:44 +0800

    Update to 2027

--- source/js/totime_2026.js
+++ source/js/totime_2026.js
@@ -1,6 +1,6 @@
 var i = 0;
 function Time(){
-    var totime = 1738080000000;
+    var totime = 1771257600000;
     var now = new Date();
     var year = now.getFullYear();
     var month = now.getMonth()+1;
@@ -27,7 +27,7 @@ function Time(){
         if (waysec <= 0) {
             document.getElementById("tovideo").href="https://www.bilibili.com/video/BV1zZ4y1d7jH"
             document.getElementById("tovideo").innerHTML="现在你可以点击它了"
-            document.getElementById("totimeinfo").innerHTML = "2025/01/29 00:00:00已过去"
+            document.getElementById("totimeinfo").innerHTML = "2026/02/16 00:00:00已过去"
         };
     };
     document.getElementById("nowtime").innerHTML="现在时间："+year+"年"+month+"月"+day+"日"+hours+":"+minutes+":"+seconds;
@@ -36,7 +36,7 @@ function Time(){
     setTimeout(topage,1000);
 };
 function Time_2() {
-    var totime_2 = 1735660800000;
+    var totime_2 = 1767196800000;
     var timestamp_2 = new Date().getTime();
     var waytime_2 = totime_2-timestamp_2;
     var wayday_2 = Math.round(waytime_2 /1000 / 60 / 60 /24);
@@ -44,7 +44,7 @@ function Time_2() {
         //waysec_2没定义，才发现整个程序在一个函数里（666）
         //整个计算程序就是一个大函数
         if (waysec_2 <= 0) {
-            document.getElementById("totimeinfo_2").innerHTML = "2025/01/01 00:00:00已过去"
+            document.getElementById("totimeinfo_2").innerHTML = "2026/01/01 00:00:00已过去"
         };
         document.getElementById("sec_2").innerHTML="秒数："+waysec_2;
         document.getElementById("day_2").innerHTML="天数："+wayday_2;
